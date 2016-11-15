b98d883bce12311729a1695caff627ea598e0eb2
BenKaren <karen_liang@163.com>
2016-11-15 20:08:20 +0800

    这是我添加的第一个测试类

---
+++ app/src/main/java/com/qianfeng/myshoppingapplication/activity/MyTestClass.java
@@ -0,0 +1,36 @@
+package com.qianfeng.myshoppingapplication.activity;
+
+/**
+ * Created by Administrator on 2016/11/15.
+ */
+
+public class MyTestClass {
+
+    private String name ;
+    private int age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
