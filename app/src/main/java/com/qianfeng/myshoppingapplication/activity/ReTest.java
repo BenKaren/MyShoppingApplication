89f8e59e7c1a5efe6ca169bb685906b7f40a5aa8
BenKaren <karen_liang@163.com>
2016-11-15 20:27:51 +0800

    这是我修改后的子线程类

---
+++ app/src/main/java/com/qianfeng/myshoppingapplication/activity/ReTest.java
@@ -0,0 +1,17 @@
+package com.qianfeng.myshoppingapplication.activity;
+
+/**
+ * Created by Administrator on 2016/11/15.
+ */
+
+public class ReTest {
+
+    public void add(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }).start();
+    }
+}
